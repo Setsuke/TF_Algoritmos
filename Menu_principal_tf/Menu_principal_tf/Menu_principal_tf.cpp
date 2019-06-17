50a204259de0acaac3a0bb00dc3c60720daaacc5
Dino Ivan Perez Vasquez <u201710880@upc.edu.pe>
2019-06-17 00:32:16 -0500

    Trabajo mejorado

--- Menu_principal_tf/Menu_principal_tf/Menu_principal_tf.cpp
+++ Menu_principal_tf/Menu_principal_tf/Menu_principal_tf.cpp
@@ -1,9 +1,13 @@
-#include "stdafx.h"
-#include <iostream>
-#include <conio.h>
+#include  "stdafx.h"
+#include  <iostream>
+#include  <conio.h>
+#include "Fila.h"
+#include "Columna.h"
+#include "Dato.h"
+#include "DF.h"
 
 using namespace System;
-using namespace std;
+using namespace  std;
 
 int menu() {
 
@@ -11,28 +15,28 @@ int menu() {
 
 	do {
 		Console::Clear();
-		cout << "\nMenu principl\n\n";
-		cout << "[1] => Cargar archivo\n";
-		cout << "[2] = >Imprimir\n";
-		cout << "[3] = >Listar\n";
-		cout << "[4] =>Guardar\n";
-		cout << "[5] = >Indexar\n";
-		cout << "[6] = >Crear nuevo data DF\n";
-		cout << "[7] = >Salir\n\n";
-		cout << "[8] =>Eligir una de la opciones: ";
+		cout << " \ n Menu principl \ n \ n ";
+		cout << " [1] => Cargar archivo \ n ";
+		cout << " [2] => Imprimir \ n ";
+		cout << " [3] => Listar \ n ";
+		cout << " [4] => Guardar \ n ";
+		cout << " [5] => Indexar \ n ";
+		cout << " [6] => Crear nuevo data DF \ n ";
+		cout << " [7] => Salir \ n \ n ";
+		cout << " [8] => Eligir una de las opciones: ";
 		cin >> op;
-	} while (!(op >= 1 && op <= 7) && (op != 7));
+	} while (!(op > = 1 && op <= 7) && (op!= 7));
 
 	return op;
 
 }
 
-int main()
+int  main()
 {
 
 	int p = menu();
 
 
 	_getch();
-    return 0;
+	return 0;
 }
